caafb68c56c32b96db5ddc64dacbba9063cd3ee3
고영균 <gyg@goyeong-gyun-ui-MacBookPro.local>
2022-06-07 15:52:48 +0900

    it can now get 10 items

--- progpkg/crawl.py
+++ progpkg/crawl.py
@@ -3,44 +3,46 @@
 import requests
 from bs4 import BeautifulSoup
 
-def crawl(addSource):
-    url = "https://www.10000recipe.com/recipe/list.html?q=" + addSource
-    recipe_url = "https://www.10000recipe.com"
-
-    page = requests.get(url)
-    soup = BeautifulSoup(page.content, "html.parser")
-
-    recipe_titles = []
-    recipe_links = []
-    recipe_contents = []
+def crawl(addSource, subList):
+    
     recipes = []
 
-    res = soup.find("ul", "rcp_m_list2")
-    titles = res.find_all("div", "common_sp_caption_tit line2")
-    links = res.find_all("a", "common_sp_link")
+    for page_number in range(1, 11):
+        url = "https://www.10000recipe.com/recipe/list.html?q=" + addSource + "&order=reco&page=" + str(page_number)
 
-    for title in titles:
-        recipe_titles.append(title.get_text())
-
-    for link in links:
-        recipe_links.append(recipe_url + link["href"])
-
-    for recipe_link in recipe_links:
-        page = requests.get(recipe_link)
+        page = requests.get(url)
         soup = BeautifulSoup(page.content, "html.parser")
 
-        res = soup.find("div", "view_step")
+        res = soup.find("ul", "rcp_m_list2")
+        titles = res.find_all("div", "common_sp_caption_tit line2")
+        links = res.find_all("a", "common_sp_link")
 
-        res = res.find_all("div", "media-body")
+        for title in titles:
+            recipes.append({"title" : title.get_text()})
 
-        recipe = ""
+        for i in range(len(links)):
+            recipes[i]["link"] = "https://www.10000recipe.com" + links[i]["href"]
+            page = requests.get("https://www.10000recipe.com" + links[i]["href"])
+            soup = BeautifulSoup(page.content, "html.parser")
 
-        for i in res:
-            recipe += i.get_text().replace("\n", "")
+            res = soup.find("div", "view_step")
 
-        recipe_contents.append(recipe)
+            res = res.find_all("div", "media-body")
 
-    for i in range(len(recipe_contents)):
-        recipes.append({"title" : recipe_titles[i], "link" : recipe_links[i], "recipe" : recipe_contents[i]})
+            recipe = ""
+
+            for j in res:
+                recipe += j.get_text().replace("\n", "")
+
+            recipes[i]["recipe"] = recipe
+
+        for i in recipes:
+            for word in subList:
+                if word in i["recipe"]:
+                    if i in recipes:
+                        recipes.remove(i)
+        
+        if len(recipes) > 9:
+            break
     
-    return recipes
+    return recipes[:10]

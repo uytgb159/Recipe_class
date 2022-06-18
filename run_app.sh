1049a4b99fbd6714d01da2b851978f45ccec8591
yoojinPPPark <101375618+yoojinPPPark@users.noreply.github.com>
2022-06-18 22:09:34 +0900

    Update run_app.sh

--- run_app.sh
+++ run_app.sh
@@ -1,17 +1,1 @@
 #!/usr/bin/sh
-
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-@app.route('/') # 접속url
-def index():
-  return render_template('home.html')
-  
-  
-@app.route('/recipe') #second page
-def recipe():
-  return render_template('recipe.html')
-
-if __name__=="__main__":
-  app.run(debug=True)

947f11875bd6aa9f8c5395f07d776c1fe789d5c7
yoojinPPPark <101375618+yoojinPPPark@users.noreply.github.com>
2022-06-20 16:13:21 +0900

    Update elastic.py

--- progpkg/elastic.py
+++ progpkg/elastic.py
@@ -21,10 +21,9 @@ es_host="http://localhost:9200"
 
 if __name__=='__main__':
     es = Elasticsearch(es_host)
-    #근데 단어 받아서 elastic 갱신한다음 random_list역시 갱신하는거는 어떻게 구현할까
     
     random_list = ['감자', '감자', '감자', '고구마', '고구마', '도마', '벡종원', '백종원', '백종원', '요거트', '요거트', '계란', '계란', '계란', '김치', '간편음식','간편음식','간편음식', '새우', '토마토', '마라'] #랜덤 단어 저장
-    #현재 세번씩 저장 되어있는 감자, 백종원, 계란, 간편음식, 두번씩 저장되어있는 고구마, 요거트, 이렇게 총 6개의 단어가 보일 것이다.
+    #현재 세번씩 저장 되어있는 감자, 백종원, 계란, 간편음식 이렇게 총 4개의 단어가 보일 것이다.
     #사용자 input을 변수 w로 받아
     w = request.form["keyword"]
     if w not in random_list:
@@ -32,15 +31,14 @@ if __name__=='__main__':
     random_list[w] += 1
     
     final_list = []
-    final_list = dict(sorted(random_list.items(), key=lambda x:x[1], reverse=True))
+    final_list = dict(sorted(random_list.items(), value=lambda x:x[1], reverse=True))
     #print(final_list[:4])
     
-    #elasticsearch에 저장
+    #elasticsearch에 다시 저장
     word=list(final_list.keys())
     cnt=list(final_list.values())
     elastic = {"words": word, "count": cnt}
     res=es.index(index='keyword', id=1, document=elastic) #아직 이 코드 이해 잘 못했어
-    #res = es.search()
     res=es.search(index='keyword', body={"query": {"match_all":{}}})
     #print(res)
     

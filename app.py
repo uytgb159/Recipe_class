83e616e19e2aa221466706cd287f2f83d1948eae
uytgb159 <rnalscjf00@naver.com>
2022-06-21 20:38:49 +0900

    changed everything...

--- app.py
+++ app.py
@@ -1,20 +1,24 @@
 #!/usr/bin/python
 
+from unittest import result
 from flask import Flask, render_template, request
-from progpkg import crawl, analysis, elastic
+from progpkg import crawl, analysis, elastic, put_in_elastic
 
 app = Flask(__name__)
 
-@app.route('/', method = ["post"]) # 접속url
-def home():
-  w = request.form["keyword"]
-  elastic.keyword(w)
-  return render_template('home.html', result1 = final_list[0], result2 = final_list[1], result3 = final_list[2], result4 = final_list[3])
+@app.route('/') # 접속url
+
+def index():
+  topword=elastic.get_top_word()
+  return render_template('home.html', result1=topword[0], result2=topword[1], result3=topword[2], result4=topword[3], result5=topword[4])
   
 @app.route('/search', methods = ["POST", "GET"]) #second page
 def recipe():
   if request.method == "POST":
     addSource = request.form["include"]
+    
+    put_in_elastic.putin(addSource) # 검색어 elasticsearch에 집어넣기
+    
     subList = list(request.form["exclude"].split())
     word_i = crawl.crawl(addSource, subList)
     tfidf = []
